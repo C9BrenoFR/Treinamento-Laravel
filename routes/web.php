2128775301ac5e825767c089e283a1f4b355ca4a
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-03-09 22:41:31 -0300

    Treinamento polimorfismo

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::middleware('customer')->group(function () {
+        Route::prefix('customer')->group(function () {
+            Route::get('/dashboard', function () {
+                return view('dashboard');
+            })->name('dashboard');
+        });
+    });
+
+    Route::middleware('admin')->group(function () {
+        Route::prefix('admin')->group(function () {
+
+            Route::get('/dashboard', function () {
+                return view('admin.dashboard');
+            })->name('admin.dashboard');
+
+            Route::get('/exibir', function() {
+                echo \Auth::user()->name . PHP_EOL;
+                echo \Auth::user()->email . PHP_EOL;
+            });
+
+        });
+    });
+    
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
